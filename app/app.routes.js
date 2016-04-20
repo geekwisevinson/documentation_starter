ba2f20fbcfd43d4dce514d379da1054c39eeca7a
vinferno <vinsonfernandez@gmail.com>
2016-04-19 18:47:16 -0700

    init

---
+++ app/app.routes.js
@@ -0,0 +1,9 @@
+angular.module('app')
+    .config(function($stateProvider,$urlRouterProvider){
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('home',{
+                url:'/home',
+                templateUrl:'home/home.html'
+            });
+    });
